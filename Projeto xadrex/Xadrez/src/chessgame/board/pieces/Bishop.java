cb209ee20804ea814ec7b7a8568be789e959cd76
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2022-10-24 16:42:15 -0300

    Added queen moviment

--- Projeto xadrex/Xadrez/src/chessgame/board/pieces/Bishop.java
+++ Projeto xadrex/Xadrez/src/chessgame/board/pieces/Bishop.java
@@ -1,6 +1,6 @@
 package chessgame.board.pieces;
+import java.util.ArrayList;
 import java.util.List;
-
 import chessgame.board.pieces.enums.Color;
 
 public class Bishop extends Piece {
@@ -15,8 +15,23 @@ public class Bishop extends Piece {
 
     @Override
     public List<Position> mover(Piece piece) {
-        // TODO Auto-generated method stub
-        return null;
+
+    List<Position> posiblePositions = new ArrayList<>();
+
+        
+            for(int i = 0 ; i <= 7; i++) {
+            if (piece.getPosition().getRow() + i <= 7 && piece.getPosition().getColumn() + i <= 7) {
+            posiblePositions.add(new Position(piece.getPosition().getRow() + i, piece.getPosition().getColumn() + i));
+            }
+            }
+            for(int i = 7; i >= 0; i--) {
+            if(piece.getPosition().getRow() - i >= 0 && piece.getPosition().getColumn() -i >= 0)  {
+                posiblePositions.add(new Position(piece.getPosition().getRow() - i, piece.getPosition().getColumn() - i));
+            }
+        }
+      
+     
+        return posiblePositions;
     }
 
     
--- Projeto xadrex/Xadrez/src/chessgame/board/pieces/Queen.java
+++ Projeto xadrex/Xadrez/src/chessgame/board/pieces/Queen.java
@@ -1,6 +1,6 @@
 package chessgame.board.pieces;
+import java.util.ArrayList;
 import java.util.List;
-
 import chessgame.board.pieces.enums.Color;
 
 public class Queen extends Piece {
@@ -15,8 +15,41 @@ public class Queen extends Piece {
 
     @Override
     public List<Position> mover(Piece piece) {
-        // TODO Auto-generated method stub
-        return null;
+
+            List<Position> posiblePositions = new ArrayList<>();
+        
+           // roller moviment
+            for(int i = piece.getPosition().getRow(); i < 7; i++) {
+            posiblePositions.add(new Position(i + 1, piece.getPosition().getColumn()));
+                
+            }
+            for(int i = piece.getPosition().getRow(); i > 0; i--) {
+            posiblePositions.add(new Position(i - 1, piece.getPosition().getColumn()));
+          
+        }
+            for(int i = piece.getPosition().getColumn(); i < 7; i++){
+                    posiblePositions.add(new Position(piece.getPosition().getRow(), i + 1 )); 
+            
+            }
+           
+            for(int i = piece.getPosition().getColumn(); i > 0 ; i--) {
+                    posiblePositions.add(new Position(piece.getPosition().getRow(), i -1 ));
+            
+            }
+
+        // bishop moviment
+             for(int i = 0 ; i <= 7; i++) {
+            if (piece.getPosition().getRow() + i <= 7 && piece.getPosition().getColumn() + i <= 7) {
+            posiblePositions.add(new Position(piece.getPosition().getRow() + i, piece.getPosition().getColumn() + i));
+            }
+            }
+            for(int i = 7; i >= 0; i--) {
+            if(piece.getPosition().getRow() - i >= 0 && piece.getPosition().getColumn() -i >= 0)  {
+                posiblePositions.add(new Position(piece.getPosition().getRow() - i, piece.getPosition().getColumn() - i));
+            }
+        }
+
+        return posiblePositions;
     }
     
     
