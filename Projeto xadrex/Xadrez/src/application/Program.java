c883c002b51c29800f2dec9c63c7e70d385c9fe2
Henrique Zapella Rocha <henri_Zap@hotmail.com>
2022-11-03 19:50:59 -0300

    More or less

--- Projeto xadrex/Xadrez/src/application/Program.java
+++ Projeto xadrex/Xadrez/src/application/Program.java
@@ -17,18 +17,22 @@ public class Program {
 
     while(true){
     System.out.println(chessRuler.borderComander());
+    System.out.println("Choose the piece you want to move ");
+    System.out.print("  Piece row: ");
     int row = sc.nextInt() ;
+    System.out.print("  Piece Collumn: ");
     int  column = sc.nextInt() ;
     if (System.getProperty("os.name").contains("Windows")) 
     new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     else 
    Runtime.getRuntime().exec("clear");
-    System.out.println("                    Possibilites: " + " " +  chessRuler.possibleMoviment(new Position(row, column)));
-    System.out.print(chessRuler.borderComander());
+    System.out.println(chessRuler.borderComander());
+    System.out.println("Possibilites: " + " " +  chessRuler.possibleMoviment(new Position(row, column)));
     System.out.println();
-    System.out.print("Chosse the new row position: ");
+    System.out.println("Chosse the new position");
+    System.out.print("new position row: ");
     int rowNew = sc.nextInt() ;
-    System.out.print("Chosse the new collumn position: ");
+    System.out.print("new position collumn: ");
     int columnNew = sc.nextInt() ;
    chessRuler.mover(new Position(row, column), new Position(rowNew, columnNew));
    if (System.getProperty("os.name").contains("Windows")) 
